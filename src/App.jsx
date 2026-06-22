edcccb1b69887e5c737e02f7f5aeb5750d213106
Marcos Soares <marcossoaress007s@gmail.com>
2026-06-21 21:44:56 -0300

    Atualizar o App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
-import { useCart } from './hooks/useCart';
+//import { useState } from 'react';
+//import { useCart } from './hooks/useCart';
 import { useToast } from './hooks/useToast';
 import Header from './components/Header';
 import Hero from './components/Hero';
-import Menu from './components/Menu';
+//import Menu from './components/Menu';
 import CartDrawer from './components/CartDrawer';
 import ToastContainer from './components/ToastContainer';
 import Footer from './components/Footer';
